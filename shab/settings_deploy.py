7df4d8886762d6e5f9add461ec57261981095920
Daxton Kamitaira <106986897+daxtonkamitaira@users.noreply.github.com>
2022-06-08 04:15:33 +0200

    Update settings_deploy.py

--- shab/settings_deploy.py
+++ shab/settings_deploy.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '29suvs$zv$%_*h#!_%6us3v6ed2i%5_nvb=d2&!$5(4_9bq=1q'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
